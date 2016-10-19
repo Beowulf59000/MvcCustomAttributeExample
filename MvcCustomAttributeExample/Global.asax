ce25b2924d298456fb0a94db2d81de6f75199e6d
Johnny <Johnny@PC-PORTABLE>
2016-10-19 20:45:56 +0200

    Ajoutez des fichiers projet.

---
+++ MvcCustomAttributeExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcCustomAttributeExample.MvcApplication" Language="C#" %>
---
+++ MvcCustomAttributeExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcCustomAttributeExample.Startup))]
+namespace MvcCustomAttributeExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

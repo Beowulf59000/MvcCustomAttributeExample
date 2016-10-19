9fe522640bb24955ef159678229b43ff5922c464
Johnny BEHAGUE <johnny.behague@live.fr>
2016-10-19 21:25:03 +0200

    Test MVC

---
+++ MvcCustomAttributeExample/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using MvcCustomAttributeExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCustomAttributeExample.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("Edit", new { id = 1 });
+        }
+
+
+        public ActionResult Edit(Int32 id)
+        {
+            TestModels test = new TestModels();
+            return this.View(test);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TestModels test)
+        {
+            if (ModelState.IsValid)
+            {
+                return this.RedirectToAction("Index");
+            }
+
+            return this.View(test);
+        }
+    }
+}
---
+++ MvcCustomAttributeExample/Models/TestModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcCustomAttributeExample.Models
+{
+    public class TestModels
+    {
+        /// <summary>
+        /// Type
+        /// </summary>
+        [Display(Name = "Type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Contenu A
+        /// </summary>
+        [Display(Name = "Contenu A")]
+        [StringLength(500, ErrorMessage = "50 caractères maximum.")]
+        public string ContenuA { get; set; }
+
+        /// <summary>
+        /// Contenu B
+        /// </summary>
+        [Display(Name = "Contenu B")]
+        [StringLength(500, ErrorMessage = "500 caractères maximum.")]
+        public string ContenuB { get; set; }
+    }
+}
